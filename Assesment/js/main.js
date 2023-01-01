15867d232265bfad3bb5644831507091ba5be6c0
Rossyfer09 <jennyfer.bonilla@gmail.com>
2023-01-02 09:47:38 +1100

    test class 2

--- Assesment/js/main.js
+++ Assesment/js/main.js
@@ -3,3 +3,14 @@ function bigger(){
     highlight.style.color ="orange";
     highlight.style.fontSize = "30px";
 }
+
+let startTime = '8 am'
+
+function logHours () {
+    let endTime = '4 Pm';
+    console.log(startTime, endTime)
+}
+
+logHours();
+
+console.log(endTime)
