57f5f71e0c375b8ff19064ebf0739af531765530
Rossyfer09 <jennyfer.bonilla@gmail.com>
2023-01-04 22:54:19 +1100

    test

--- Assesment/js/main.js
+++ Assesment/js/main.js
@@ -9,12 +9,18 @@
 // shows error as entime can be reach outside the function logHours
 //console.log(endTime);
 
-
-let laila = 7;
+// 3 operrand example//
+/*let laila = 7;
 let yumi = 14;
 console.log (yumi >= 7 && yumi <= 12);
 
 let age = 36;
 const schoolofCode = (age >18) ? "You are in!" : "Check out code camps..";
-console.log(schoolofCode);
+console.log(schoolofCode);*/
+
+//lopps example
+for (let winner =4; winner < 10; winner++){
+    console.log("we have a " + winner + " the winner");
+}
+
 
